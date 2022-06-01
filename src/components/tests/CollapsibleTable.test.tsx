bb8a295096aa6bc0c8320fc14997003c6488edd1
Milos Cemalovic <Milos.Cemalovic@rs.ey.com>
2022-06-01 12:02:56 +0200

    Test CollapsibleTable component

--- src/components/tests/CollapsibleTable.test.tsx
+++ src/components/tests/CollapsibleTable.test.tsx
@@ -1,6 +1,11 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import CollapsibleTable from '../CollapsibleTable'
 
 test('should have table', () => {
   render(<CollapsibleTable />)
+
+  const table = screen.getByRole('table')
+  const tableRows = screen.getAllByRole('row')
+  expect(table).toBeInTheDocument()
+  expect(tableRows).toHaveLength(9)
 })
