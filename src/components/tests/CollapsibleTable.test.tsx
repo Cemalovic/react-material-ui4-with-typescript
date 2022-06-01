3b7e349a0f6b68ccc081e40e8b6fce4cfe8826fa
Milos Cemalovic <Milos.Cemalovic@rs.ey.com>
2022-06-01 15:35:37 +0200

    find another table on button/arrow click

--- src/components/tests/CollapsibleTable.test.tsx
+++ src/components/tests/CollapsibleTable.test.tsx
@@ -1,11 +1,21 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import CollapsibleTable from '../CollapsibleTable'
 
-test('should have table', () => {
+test('should have table, rows and find another table on button click', async () => {
   render(<CollapsibleTable />)
 
-  const table = screen.getByRole('table')
+  const collapsibleTable = screen.getByRole('table', {
+    name: /collapsible table/
+  })
   const tableRows = screen.getAllByRole('row')
-  expect(table).toBeInTheDocument()
+  expect(collapsibleTable).toBeInTheDocument()
   expect(tableRows).toHaveLength(9)
+
+  const buttonArrow = screen.getAllByRole('button', { name: /expand row/ })
+  expect(buttonArrow).toHaveLength(4)
+
+  userEvent.click(buttonArrow[0])
+  const accordianTable = await screen.findByRole('table', { name: /purchases/ })
+  expect(accordianTable).toBeInTheDocument()
 })
